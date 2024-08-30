0fad1a229f3263e1f645adcc9d5a750aed095bba
StudenV <va75127@gmail.com>
2024-08-30 22:17:15 +0300

    commit 3

--- ibspractice/src/test/java/Cucumber/steps/MyStepdefs.java
+++
@@ -1,9 +0,0 @@
-package Cucumber.steps;
-
-import io.cucumber.java.ru.Когда;
-
-public class MyStepdefs {
-    @Когда("Я удаляю товар с ID {int}")
-    public void яУдаляюТоварСID(int arg0) {
-    }
-}
